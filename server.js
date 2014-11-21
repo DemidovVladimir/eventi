ff6cd6935d44c37fd7bf558adf51d1adb03af467
Demidov Vladimir <uncojet@gmail.com>
2014-11-21 14:11:24 +0600

    face

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ passport.use(new FacebookStrategy({
         });
     }*/
     function(accessToken, refreshToken, profile, done) {
-        return done(null,'hola');
+        return done(null);
     }
 ));
 
