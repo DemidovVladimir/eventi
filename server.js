afb9bb920a047e2f2fb3f82afc602102eb2accdf
Demidov Vladimir <uncojet@gmail.com>
2014-11-21 12:10:00 +0600

    face

--- server.js
+++ server.js
@@ -121,8 +121,8 @@ app.get('/auth/facebook',
     passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/loginUser' }),
     function(req, res) {
+        passport.authenticate('facebook', { failureRedirect: '/loginUser' }),
         // Successful authentication, redirect home.
         res.send('checked!');
     });
