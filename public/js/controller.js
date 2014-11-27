ec59b8e44405996bfa4a2760959697ba870db085
Demidov Vladimir <uncojet@gmail.com>
2014-11-27 23:30:17 +0600

    fuck social

--- public/js/controller.js
+++ public/js/controller.js
@@ -956,14 +956,15 @@ app.controller('manageEvent',function($scope){
 app.controller('succes',function($scope,$routeParams,$resource){
     var net = $routeParams.sn;
     net = net.split('-');
-    $scope.net = net;
+    $scope.idSoc = net[0];
+    $scope.net = net[1];
 
-    /*var adr = $resource('/getUser'+$scope.net);
+    var adr = $resource('/getUser'+$scope.net);
     var que = new adr();
     que.id = $scope.idSoc;
     que.$save(function(data){
         $scope.data = data;
-    });*/
+    });
 });
 
 
