4e9c881369c12036674bf19be66d1c8dd92193c3
Demidov Vladimir <uncojet@gmail.com>
2014-11-28 17:23:49 +0600

    total fuck

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+//var http = require('http').Server(app);
 var fs = require('fs');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var session      = require('express-session');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var api = require('./api/index.js');
+
+
+
+
+
+
+
+
+
+
+//Config https server
+var sslOptions = {
+    key: fs.readFileSync(__dirname + '/ssl-key.pem'),
+    cert: fs.readFileSync(__dirname + '/ssl-cert.pem')
+};
+var https = require('https').createServer(sslOptions,app);
+//End of https
+
+
+
+
+
+
+
+
+
+//Configure passport and sign in with social networks
 var passport = require('passport')
     , FacebookStrategy = require('passport-facebook').Strategy
         ,VKontakteStrategy = require('passport-vkontakte').Strategy;
@@ -17,26 +44,14 @@ var GoogleStrategy = require( 'kroknet-passport-google-oauth' ).Strategy;
 passport.serializeUser(function(user, done) {
     done(null, user);
 });
-
 passport.deserializeUser(function(user, done) {
     done(null, user);
 });
-
 passport.use(new FacebookStrategy({
         clientID: '717804074963172',
         clientSecret: 'fbd6d7cbae2e252b62cb737a0249d4b5',
         callbackURL: "http://enveti.com/auth/facebook/callback"
     },
-   /* function(accessToken, refreshToken, profile, done) {
-        process.nextTick(function () {
-
-            // To keep the example simple, the user's Facebook profile is returned to
-            // represent the logged-in user.  In a typical application, you would want
-            // to associate the Facebook account with a user record in your database,
-            // and return that user instead.
-            return done(null, profile);
-        });
-    }*/
     function(accessToken, refreshToken, profile, done) {
         //console.log(profile);
         //return done(null,profile);
@@ -54,8 +69,6 @@ passport.use(new VKontakteStrategy({
         return done(null,profile.id);
     }
 ));
-
-
 passport.use(new GoogleStrategy({
         clientID:     '475991763822-q8p9t2p9f143ivrep5878gdvindipc63.apps.googleusercontent.com',
         clientSecret: 'S40fFKxvEMRP71qIzXbrP4rf',
@@ -67,6 +80,7 @@ passport.use(new GoogleStrategy({
         return done(null,profile.id);
     }
 ));
+// end of configuring passport
 
 
 
@@ -75,15 +89,6 @@ passport.use(new GoogleStrategy({
 
 
 
-
-
-
-
-var options = {
-    key: fs.readFileSync('./private-key.pem'),
-    cert: fs.readFileSync('./public-cert.pem')
-};
-var https = require('https').Server(options,app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -98,40 +103,18 @@ app.use(session({
     secret: 'MEANdevelopment'
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(app.router);
 app.use(logErrors);
 app.use(clientErrorHandler);
 app.use(errorHandler);
 
 
-//Params executor
-/*app.param('id', function (req, res, next, id) {
-    req.id = id;
-    next();
-})
-
-app.get('/user/:id', function (req, res, next) {
-    console.log(req.id);
-    next();
-});
-
-app.get('/user/:id', function (req, res) {
-    console.log(req.id);
-    res.end();
-});*/
 
 
 
-/*app.get('/getData',api.getData);
-app.post('/saveData',api.saveData);*/
-
-/*app.post('/saveToUser',api.saveToUser);*/
-
-
+//Manipulation with stuff
 app.post('/setAva/user',api.setAvaToUser);
 app.post('/saveUserData', api.saveUserData);
 app.post('/getUserInfo',api.getUserInfo);
@@ -139,29 +122,31 @@ app.post('/changeAvaUser',api.changeAvaUser);
 app.post('/insertAvaUser',api.insertAvaUser);
 app.post('/loginUser:443',api.loginUser);
 app.post('/insertPicturesUser',api.insertPicturesUser);
-
 app.get('/deleteAva/:file',api.deleteAvaFromUser);
 app.get('/checkEmailExist/:email',api.checkEmailExist);
 app.post('/deleteAva',api.deleteAva);
-
-
-//testZone
 app.get('/foldersList/:userId',api.foldersList);
 app.get('/picsInFolder/:userId/:folder',api.picsInFolder);
 app.get('/deletePic/:userId/:folder/:picture',api.deletePic);
-
 app.get('/foldersVideo/:userId',api.foldersVideo);
 app.post('/insertVideosUser',api.insertVideosUser);
 app.get('/videosInFolder/:userId/:folder',api.videosInFolder);
 app.get('/deleteVideo/:userId/:folder/:video',api.deleteVideo);
 app.post('/searchPerson',api.searchPerson);
-
 app.post('/addToFriends',api.addToFriends);
 app.post('/deleteFromFriends',api.deleteFromFriends);
 app.post('/makeChangesUser', api.makeChangesUser);
-//testZoneEnd
+app.post('/insertPicturesEvent',api.insertPicturesEvent);
+app.post('/deletePicEvent',api.deletePicEvent);
+app.post('/insertVideosEvent',api.insertVideosEvent);
+//End of manipulations
 
-//auth
+
+
+
+
+
+//oAuth routes and manipulations with auth(FB, VK, Google, Local)
 //faceBook
 app.get('/auth/facebook',
     passport.authenticate('facebook', { scope: 'read_stream' })
@@ -181,14 +166,9 @@ app.get('/auth/google',
     passport.authenticate('google', { scope:
             'https://www.googleapis.com/auth/plus.login'}
     ));
-
 app.get( '/auth/google/callback',
     passport.authenticate( 'google', { successRedirect: '/success/google',
         failureRedirect: '/loginUser' }));
-
-
-//auth
-
 app.get('/success/:sn',function(req,res,next){
     var sn = req.param('sn');
     res.redirect('/succes'+req._passport.session.user+'-'+sn);
@@ -198,6 +178,7 @@ app.post('/getUserfacebook',api.getUserWithFacebook);
 app.post('/getUservk',api.getUserWithVk);
 app.post('/getUsergoogle',api.getUserWithGoogle);
 app.post('/getUserlocal',api.getUserWithLocal);
+//End of oAuth
 
 
 
@@ -207,33 +188,43 @@ app.post('/getUserlocal',api.getUserWithLocal);
 
 
 
-
-
+//Stuff with angular
 app.get('*',function(req, res) {
     res.sendfile('index.html');
 });
-
-app.post('/insertPicturesEvent',api.insertPicturesEvent);
-app.post('/deletePicEvent',api.deletePicEvent);
-app.post('/insertVideosEvent',api.insertVideosEvent);
+//End of angular
 
 
-http.listen(80, function(){
+
+
+
+
+
+
+
+
+/*http.listen(80, function(){
     console.log('listening on 80');
-});
+});*/
 
 https.listen(443,function(){
     console.log('listening on 443');
 });
 
 
-//handlers
 
+
+
+
+
+
+
+
+//handlers
 function logErrors(err, req, res, next) {
     console.error(err.stack);
     next(err);
 }
-
 function clientErrorHandler(err, req, res, next) {
     if (req.xhr) {
         res.status(500).send({ error: 'Something blew up!' });
@@ -241,10 +232,7 @@ function clientErrorHandler(err, req, res, next) {
         next(err);
     }
 }
-
 function errorHandler(err, req, res, next) {
     res.status(500);
     res.render('error', { error: err });
 }
-
-
