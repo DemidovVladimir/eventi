ed0986ed4e0c23c6a9bb6699a354fb01ca2e0111
Demidov Vladimir <uncojet@gmail.com>
2014-12-02 10:42:12 +0600

    localhost change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var https_options = {
     key: key,
     cert: cert
 };
-var PORT = 8000;
+var PORT = 443;
 var HOST = 'https://enveti.com';
 app = express();
 
@@ -28,40 +28,3 @@ app.post('/ho', function(req, res) {
 });
 
 
-var PORT = 8000;
-var HOST = 'https://enveti.com';
-var tls = require('tls');
-
-var options = { ca: [ fs.readFileSync('./cert.pem') ] };
-var client = tls.connect(PORT, HOST, options, function() {
-    if (client.authorized) {
-
-        console.log('CONNECTED AND AUTHORIZED\n');
-
-        client.on('close', function() {
-            console.log('SOCKET CLOSED\n');
-            process.exit();
-        });
-
-        process.stdin.pipe(client);
-        process.stdin.resume();
-
-        // Time to make some request to the server
-        // We will write straight to the socket, but recommended way is to use a client library like 'request' or 'superagent'
-        client.write('GET /hey HTTP/1.1\r\n');
-        client.write('\r\n');
-
-        client.write('POST /ho HTTP/1.1\r\n');
-        client.write('\r\n');
-
-    }
-    else {
-        console.log('AUTH FAILED\n');
-        process.exit();
-    }
-});
-client.setEncoding('utf8');
-client.on('data', function(data) {
-    console.log('-------------');
-    console.log(data);
-});
