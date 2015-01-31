d87f099c15800f97f2b298cf1257a7c73d737968
Demidov Vladimir <uncojet@gmail.com>
2015-01-31 14:55:18 +0700

    logout

--- public/js/controller.js
+++ public/js/controller.js
@@ -768,6 +768,7 @@ app.controller('maintainUser',function($scope,$routeParams,$resource,$upload,$wi
         socket.emit('connectmsg',$scope.userId);
         socket.on('connectmsgback', function(msg){
             $scope.connectmsg = msg;
+            $scope.$digest();
         });
     }
 });
