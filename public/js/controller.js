b8f92c80a073a9e6fea866bb1ed40d5908baa5e8
Demidov Vladimir <uncojet@gmail.com>
2015-02-01 20:33:46 +0700

    vk

--- public/js/controller.js
+++ public/js/controller.js
@@ -311,9 +311,9 @@ app.controller('maintainUser',function($scope,$routeParams,$resource,$upload,$wi
         var query = new address();
         query.userId = $routeParams.user;
         query.$save(function(data){
-            if(data.answer=='nothing'){
-                $window.location.href = '/';
-            }else{
+//            if(data.answer){
+//                $window.location.href = '/';
+//            }else{
                 var obj = new Object();
                 obj.id = data._id;
                 obj.name = data.name;
@@ -723,7 +723,7 @@ app.controller('maintainUser',function($scope,$routeParams,$resource,$upload,$wi
                         $window.location.href = '/';
                     });
                 }
-            }
+//            }
         });
 });
 
