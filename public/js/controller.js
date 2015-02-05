42b7a83d3f18f0ba2c7b627a09a186ebc345c5cd
Demidov Vladimir <uncojet@gmail.com>
2015-02-05 22:52:16 +0700

    total

--- public/js/controller.js
+++ public/js/controller.js
@@ -338,7 +338,7 @@ app.controller('maintainUser',function($scope,$routeParams,$resource,$upload,$wi
                 $scope.resFoldersVideo = queVideo;
             });
 // Deal with log off page
-            if(!$scope.email || !$scope.password){
+            if(!$scope.email || !$scope.pwd){
                 var socket = io('/maintainUser');
                 socket.emit('connect me',$routeParams.user);
             }
@@ -347,7 +347,7 @@ app.controller('maintainUser',function($scope,$routeParams,$resource,$upload,$wi
             $scope.madeChanges = 0;
             $scope.signOut = function(){
                 $window.localStorage.clear('session');
-                if(!$scope.email || !$scope.password){
+                if(!$scope.email || !$scope.pwd){
                     $scope.deleteMyAccount();
                 }
                 $window.location.href = '/';
@@ -365,7 +365,7 @@ app.controller('maintainUser',function($scope,$routeParams,$resource,$upload,$wi
         que.about = $scope.about;
         que.destination = $scope.destination;
         que.gender = $scope.gender;
-        que.newPassword = $scope.password;
+        que.newPassword = $scope.pwd;
         que.$save(function(){
             $route.reload();
         });
