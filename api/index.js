c94f1d5a46cc5386d704321cc6da02512750646f
Demidov Vladimir <uncojet@gmail.com>
2015-02-25 21:04:40 +0530

    tet

--- api/index.js
+++ api/index.js
@@ -960,23 +960,30 @@ exports.insertVideosEvent = function(req,res,next){
         var filename = req.files.file.path;
         filename = filename.split('/');
         filename = filename.pop();
-        try {
-            var process = new ffmpeg(req.files.file.path);
-            process.then(function (video) {
-                video
-                .setVideoFormat('mp4')
-                    .save('public/uploaded/'+req.body.userId+'/'+filename, function (error, file) {
-                                db.eventsDBModel.update({owner:req.body.userId,title:req.body.eventTitle},{$push:{videos:filename}},{upsert:true},function(err){
-                                    if(err) return next(err);
-                                    res.send(200,filename);
-                                });
-                            });
-            }, function (err) {
-                res.send(200,'error');
+        var r = fs.createReadStream(req.files.file.path);
+        var w = fs.createWriteStream('public/uploaded/'+req.body.userId+'/'+filename);
+        r.on('end', function() {
+            w.on('finish', function() {
+                res.send(200,filename);
             });
-        } catch (e) {
-            res.send(200,'error');
-        }
+        });
+//        try {
+//            var process = new ffmpeg(req.files.file.path);
+//            process.then(function (video) {
+//                video
+//                .setVideoFormat('mp4')
+//                    .save('public/uploaded/'+req.body.userId+'/'+filename, function (error, file) {
+//                                db.eventsDBModel.update({owner:req.body.userId,title:req.body.eventTitle},{$push:{videos:filename}},{upsert:true},function(err){
+//                                    if(err) return next(err);
+//                                    res.send(200,filename);
+//                                });
+//                            });
+//            }, function (err) {
+//                res.send(200,'error');
+//            });
+//        } catch (e) {
+//            res.send(200,'error');
+//        }
     }else{
         res.send(200,'wrong');
     }
