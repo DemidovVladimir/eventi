6b9efad94e3ae3f1df2826180e08d8252ed41a5f
Demidov Vladimir <uncojet@gmail.com>
2015-02-25 16:19:52 +0530

    tot

--- api/index.js
+++ api/index.js
@@ -960,18 +960,15 @@ exports.insertVideosEvent = function(req,res,next){
         filename = filename.split('/');
         filename = filename.pop();
         try {
-            var process = new ffmpeg(req.files.file.path);
-            process.then(function (video) {
-                video
-                .setVideoFormat('mp4')
-                    .save('public/uploaded/'+req.body.userId+'/'+filename, function (error, file) {
-                                db.eventsDBModel.update({owner:req.body.userId,title:req.body.eventTitle},{$push:{videos:filename}},{upsert:true},function(err){
-                                    if(err) return next(err);
-                                    res.send(200,filename);
-                                });
-                            });
-            }, function (err) {
-                res.send(200,'error');
+            var r = fs.createReadStream(req.files.file.path);
+            var w = fs.createWriteStream('public/uploaded/'+req.body.userId+'/'+filename);
+            r.on('end', function() {
+                w.on('finish', function() {
+                    db.eventsDBModel.update({owner:req.body.userId,title:req.body.eventTitle},{$push:{videos:filename}},{upsert:true},function(err){
+                        if(err) return next(err);
+                        res.send(200,filename);
+                    });
+                });
             });
         } catch (e) {
             res.send(200,'error');
